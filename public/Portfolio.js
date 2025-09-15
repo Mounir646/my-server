be6bebf5478a1a8d804f4c8c60f53b117d8de913
Mounir asrih <asrih.mounir@etu.uae.ac.ma>
2025-09-15 23:08:41 +0200

    Update mobile menu and CSS

---
+++ public/Portfolio.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x8e44ad,       // Globe lines
+  color2: 0x00b894,      // Points
+  backgroundColor: 0x1e1e2f
+});
+
---
+++ public/get.js
@@ -0,0 +1,7 @@
+
+  const btn = document.getElementById("getStarted");
+  btn.onclick = function() {
+    // hna smi l file HTML dyalek li bghiti tredirecti lih
+    window.location.href = "monportfolie.html"; // ila f nafs dossier
+  };
+
